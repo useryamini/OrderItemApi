a9995adc4525b67b8ce05d7c9a6c5c684f32c596
useryamini <96192360+useryamini@users.noreply.github.com>
2021-12-26 22:31:20 +0530

    Add files via upload

---
+++ OrderItem/OrderItem/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrderItem.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace OrderItem.Controllers
+{
+    public class OrderController : Controller
+    {
+        public List<Cart> cart;
+
+        [Route("api/[controller]/[action]")]
+        [HttpPost]
+        public IActionResult GetCart([FromBody] int MId)
+        {
+            using var client = new HttpClient();
+
+            string uri = $"http://localhost:52101/id?id={MId}";
+
+            var response = client.GetAsync(uri).Result;
+            if (response != null)
+            {
+
+                var details = response.Content.ReadAsStringAsync().Result;
+                Cart storing = JsonConvert.DeserializeObject<Cart>(details);
+
+
+                cart = new List<Cart>()
+                {
+                    new Cart()
+                    {
+                       CartId = 1,
+                       UserId = 1,
+                       Id = storing.Id,
+                       Name = storing.Name
+
+                    }
+
+                };
+
+            }
+            return Ok(cart);
+
+        }
+    }
+}
---
+++ OrderItem/OrderItem/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItem.Models
+{
+    public class Cart
+    {
+       
+
+        [Key]
+        public int CartId { get; set; }
+        public int UserId { get; set; }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
